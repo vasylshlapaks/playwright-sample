261b58819b7f13c36a7c63de30ddf5e3761fd295
Vasil Shlapak <vasilsh@titanium-tech.net>
2022-06-03 14:32:04 +0400

    Updated framework

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,44 +1,34 @@
-// playwright.config.ts
 import { PlaywrightTestConfig } from '@playwright/test';
+import {sessionStorageNames} from "./testData/sessionFileNames";
+
 const config: PlaywrightTestConfig = {
-  // Put any shared options on the top level.
-
+  globalTimeout: 710000,
+  timeout: 210000,
+  retries: 0,
+  reporter: 'list',
+  testDir: './tests',
+  globalSetup: './setup/globalSetup.ts',
+  outputDir: './test-results',
+  workers: 2,
   use: {
-    // Browser options
-    headless: true,
-    // slowMo: 50,
-    timeout: 50000,
-    // Context options
-    viewport: { width: 1920, height: 1080 },
-    ignoreHTTPSErrors: true,
+    baseURL: process.env.E2E_URL || 'http://localhost:3000',
+    browserName: 'chromium',
+    viewport: { width: 1600, height: 1200 },
+    headless: false,
+    video: 'retain-on-failure',
     screenshot: 'only-on-failure',
-    video: 'retry-with-video',
-
-    // Artifacts
+    storageState: `sessions/${sessionStorageNames.userForLogin}`,
+    actionTimeout: 40000,
+    navigationTimeout: 40000,
+    permissions: ['clipboard-read', 'clipboard-write'],
   },
-
-  projects: [
-    {
-      name: 'Chromium',
-      retries: 2,
-      timeout: 50000,
-      use: {
-        // Configure the browser to use.
-        browserName: 'chromium',
-        screenshot: 'only-on-failure',
-        video: 'retry-with-video',
-      },
-    },
-
-    // {
-    //   name: 'Firefox',
-    //   use: { browserName: 'firefox' },
-    // },
-
-    // {
-    //   name: 'WebKit',
-    //   use: { browserName: 'webkit' },
-    // },
-  ],
+  expect: {
+    timeout: 35000,
+    toMatchSnapshot: {
+      maxDiffPixels: 30
+    }
+  },
+  snapshotDir: 'snapshots',
 };
+
 export default config;
