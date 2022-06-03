d1f6cd58bce0d39d2b9a07d5bee55fd63b1f5a28
Vasil Shlapak <vasilsh@titanium-tech.net>
2022-06-03 16:40:25 +0400

    Updated config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,23 +1,21 @@
 import { PlaywrightTestConfig } from '@playwright/test';
-import {sessionStorageNames} from "./testData/sessionFileNames";
 
 const config: PlaywrightTestConfig = {
   globalTimeout: 710000,
   timeout: 210000,
-  retries: 0,
+  retries: process.env.E2E_CI ? 2 : 0,
   reporter: 'list',
   testDir: './tests',
   globalSetup: './setup/globalSetup.ts',
   outputDir: './test-results',
   workers: 2,
   use: {
-    baseURL: process.env.E2E_URL || 'http://localhost:3000',
+    baseURL: process.env.E2E_URL || 'https://app.uniswap.org/',
     browserName: 'chromium',
     viewport: { width: 1600, height: 1200 },
     headless: false,
     video: 'retain-on-failure',
     screenshot: 'only-on-failure',
-    storageState: `sessions/${sessionStorageNames.userForLogin}`,
     actionTimeout: 40000,
     navigationTimeout: 40000,
     permissions: ['clipboard-read', 'clipboard-write'],
