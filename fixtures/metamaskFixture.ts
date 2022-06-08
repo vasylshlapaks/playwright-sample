ce6bebab325958b8cb7cfde1f9a6a4dcd646702a
Vasil Shlapak <vasilsh@titanium-tech.net>
2022-06-08 17:49:11 +0400

    Updated fixture

--- fixtures/metamaskFixture.ts
+++ fixtures/metamaskFixture.ts
@@ -5,12 +5,13 @@ import {WebPage} from "../page-object/webPage";
 
 type ixsFixtures = {
   connectWalletPage: ConnectWalletPage;
+  connectedWalletPage: ConnectWalletPage;
   metamaskPage: MetamaskPage,
   webPage: WebPage
 };
 
 export const test = base.extend<ixsFixtures>({
-  browser: async ({ browser }, use) => {
+  context: [async ({ browser }, use) => {
     const pathToExtension = require('path').join(__dirname, '..', 'extensions/metamaskFiles-master');
     const userDataDir = '';
 
@@ -28,18 +29,19 @@ export const test = base.extend<ixsFixtures>({
     ]);
 
     await use(browserContext);
-  },
+    await browser.close();
+  }, { scope: 'test' }],
 
   // Overwriting of page for using it in the same browser with metamask
-  page: async ({ browser, baseURL }, use) => {
-    const metamaskContextPage = await browser.newPage();
+  page: async ({ context, baseURL }, use) => {
+    const metamaskContextPage = await context.newPage();
 
     await metamaskContextPage.goto(baseURL);
     await use(metamaskContextPage);
   },
 
-  metamaskPage: async ({ page, browser }, use) => {
-    const pageWithMetamask = await browser.pages()[1];
+  metamaskPage: async ({ context }, use) => {
+    const pageWithMetamask = await context.pages()[1];
     const metamaskPage = new MetamaskPage(pageWithMetamask);
 
     await metamaskPage.fullyLoginToMetamask(process.env.METAMASK_RECOVERY, process.env.METAMASK_PASSWORD);
@@ -47,11 +49,17 @@ export const test = base.extend<ixsFixtures>({
     await use(metamaskPage);
   },
 
-  connectWalletPage: async ({ page, browser }, use) => {
-    await use(new ConnectWalletPage(page, browser));
+  connectWalletPage: async ({ page, context }, use) => {
+    await use(new ConnectWalletPage(page, context));
   },
 
-  webPage: async ({ page, browser }, use) => {
-    await use(new WebPage(page, browser));
-  }
+  webPage: async ({ page, context }, use) => {
+    await use(new WebPage(page, context));
+  },
+
+  connectedWalletPage: async ({ connectWalletPage, metamaskPage }, use) => {
+    await connectWalletPage.connectMetaMask();
+
+    await use(connectWalletPage);
+  },
 });
