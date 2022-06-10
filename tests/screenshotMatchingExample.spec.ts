04276f8b8b76c773573ce4e4744a72c265b7ded6
Vasil Shlapak <vasilsh@titanium-tech.net>
2022-06-10 16:31:02 +0400

    Added example of test

--- tests/screenshotMatchingExample.spec.ts
+++ tests/screenshotMatchingExample.spec.ts
@@ -1,14 +1,13 @@
-import { expect } from '@playwright/test';
 import { test } from '../fixtures/metamaskFixture';
 
 test.describe('Check UI matching of elements', () => {
-  test(`Check snapshot of single element`, async ({ page, connectWalletScreen, topNavigationBar, webPage }) => {
+  test(`Check snapshot of single element`, async ({ page, connectWalletScreen, webPage }) => {
     await connectWalletScreen.connectWalletButton.click();
 
     await webPage.assertElementScreenshotMatchToSnapshot(connectWalletScreen.connectViaMetamaskButton);
   });
 
-  test(`Check snapshot of full page`, async ({ page, connectWalletScreen, topNavigationBar, webPage }) => {
+  test(`Check snapshot of full page`, async ({ page, connectWalletScreen, webPage }) => {
     await connectWalletScreen.connectWalletButton.click();
 
     await webPage.assertPageScreenshotMatchToSnapshot(page);
