5a48a11aa7974a193d7036a027289c5f81c09bd2
Vasil Shlapak <vasilsh@titanium-tech.net>
2022-06-15 15:08:18 +0400

    Added examples of api requests

---
+++ tests/apiCallsExample.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+import {addUser, getUsersList} from "../helpers/api/helpers";
+
+test.describe('GET and POST requests example', () => {
+  test('Test ability to get request', async ({}) => {
+    const emailOfUser = 'george.edwards@reqres.in';
+    const usersList = await getUsersList();
+
+    await expect(usersList).toContain(emailOfUser);
+  });
+
+  test('Test ability to post request', async ({}) => {
+    const user = {
+      name: 'test',
+      job: 'qa'
+    };
+    const newUser = await addUser(user);
+
+    await expect(newUser).toContain(user.name);
+    await expect(newUser).toContain(user.job);
+  });
+});
