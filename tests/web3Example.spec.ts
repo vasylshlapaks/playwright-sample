d2912e61f205275ea8607e226d332881f7480605
Vasil Shlapak <vasilsh@titanium-tech.net>
2022-06-17 20:44:58 +0400

    Updated titles

--- tests/web3Example.spec.ts
+++ tests/web3Example.spec.ts
@@ -1,10 +1,10 @@
 import  {expect} from "@playwright/test";
 import { Web3Helpers} from '../helpers/web3/web3'
-import { test } from '../fixtures/metamaskFixture';
+import { test } from '../fixtures/baseFixture';
 import {metamaskWallet} from "../helpers/web3/metamaskData";
 
-test.describe('Check connection of wallet', () => {
-  test(`Check the ability to connect Metamask`, async ({ page }) => {
+test.describe('Check sending of crypto via web3', () => {
+  test(`Check sending of kova via web3`, async ({ page }) => {
     const web3 = new Web3Helpers();
     const secondWallet = '0xe2943d1fc805bea8b8b39f3a5c0e1e53ba8c945a';
     const valueToSend = 0.00001;
