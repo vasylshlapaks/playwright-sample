aafbd4d20c2fe42a9de88ce715601bfa28b27350
Vasil Shlapak <vasilsh@titanium-tech.net>
2022-07-05 12:06:27 +0400

    Enabled tests

--- tests/connectMetamaskExample.spec.ts
+++ tests/connectMetamaskExample.spec.ts
@@ -2,7 +2,7 @@ import { expect } from '@playwright/test';
 import { test } from '../fixtures/metamaskFixture';
 
 test.describe('Check connection of wallet', () => {
-  test.skip(`Check the ability to connect Metamask`, async ({ page, connectWalletScreen, topNavigationBar }) => {
+  test(`Check the ability to connect Metamask`, async ({ page, connectWalletScreen, topNavigationBar }) => {
     await connectWalletScreen.connectMetaMask();
 
     await expect(topNavigationBar.farmingLink).toBeVisible();
